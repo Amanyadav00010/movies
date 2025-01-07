360c00ea020619b99b9f2d9c87c1c6cd60ad7883
Amanyadav00010 <hello.zen60@gmail.com>
2025-01-07 11:05:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7944434973:AAENwb8ouKSa_eV36v_gUYCJxxv1RjO
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5693566725').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/NOBiTA_SiZUKA_DOREMON") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Free_movies_0001_bot") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002457590437'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002066155266').split()]
